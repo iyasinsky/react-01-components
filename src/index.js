453cc457942f16601faa30a1676da4dbde339e98
Igor Yasinsky <iyasinskyy@gmail.com>
2023-08-31 18:16:25 +0300

    deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { ThemeProvider } from 'styled-components';
+// import styled from 'styled-components'
 import { App } from 'components/App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
